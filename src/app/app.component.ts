dba4ee44c7f846d3a4c4899645a741997ade8bc0
iamrohitpc555 <35221090+iamrohitpc555@users.noreply.github.com>
2021-04-24 18:17:50 +0530

    Added Functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Helloworld';
+  imgUrl = "../assets/BridgeLabz-2.jpg";
+  url = "https://www.bridgelabz.com";
+  //Specify userName as String Type
+  userName: string = "";
+  nameError: string = "";
+
+  ngOnInit(): void {
+    this.title = "Hello from Bridgelabz!!";
+  }
+
+  onClick($event) {
+    console.log("Save button is clicked!", $event);
+    window.open(this.url, "_blank");
+  }
+
+  onInput($event) {
+    console.log("Change Event Occured! ", $event.data);
+    const nameRegex = RegExp('^[A-Z]{1}[a-zA-Z\\s]{2,}$');
+    if (nameRegex.test(this.userName)) {
+      this.nameError = "";
+      return;
+    }
+    this.nameError = "Name is incorrect!";
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+// Importing Form Module allows us to use Angular Form Elements and bind them to the Model of the App Component
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -10,6 +11,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
